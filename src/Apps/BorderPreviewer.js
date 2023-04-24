764c58d1628c8536b52bc30fc3a545a7a9add23f
nxnikolaos <98708019+nxnikolaos@users.noreply.github.com>
2023-04-24 22:39:04 +0300

    Cleaned up by using child components.

--- src/Apps/BorderPreviewer.js
+++ src/Apps/BorderPreviewer.js
@@ -10,57 +10,21 @@ const BorderPreviewer = () => {
   return (
     <>
       <h1>This is a border radius previewer!</h1>
-      <RadiusButton position={"Top"} align={"Left"}></RadiusButton>
-      <label>
-        Top Left Radius
-        <input
-          value={topLRadius}
-          onChange={(e) => setTopLRadius(Number(e.target.value))}
-          type="number"
-          size={2}
-          name="topLeftRadius"
-        ></input>
-      </label>
-      <label className="float-right">
-        Top Right Radius
-        <input
-          value={topRRadius}
-          onChange={(e) => setTopRRadius(Number(e.target.value))}
-          type="number"
-          size={2}
-          name="topRightRadius"
-        ></input>
-      </label>
+      <RadiusButton setValue={setTopLRadius} value={topLRadius} position={"Top"} align={"Left"}></RadiusButton>
+      <RadiusButton setValue={setTopRRadius} value={topRRadius} position={"Top"} align={"Right"}></RadiusButton>
+
       <div
         className="box"
         style={{
           borderTopLeftRadius: topLRadius,
-          //   backgroundColor: "red",
           borderTopRightRadius: topRRadius,
           borderBottomLeftRadius: botLRadius,
           borderBottomRightRadius: botRRadius,
         }}
       ></div>
-      <label>
-        Bottom Left Radius
-        <input
-          value={botLRadius}
-          onChange={(e) => setBotLRadius(Number(e.target.value))}
-          type="number"
-          size={2}
-          name="bottomLeftRadius"
-        ></input>
-      </label>
-      <label className="float-right">
-        Bottom Right Radius
-        <input
-          value={botRRadius}
-          onChange={(e) => setBotRRadius(Number(e.target.value))}
-          type="number"
-          size={2}
-          name="bottomRightRadius"
-        ></input>
-      </label>
+      <RadiusButton setValue={setBotLRadius} value={botLRadius} position={"Bottom"} align={"Left"}></RadiusButton>
+      <RadiusButton setValue={setBotRRadius} value={botRRadius} position={"Bottom"} align={"Right"}></RadiusButton>
+
       <p style={{ textAlign: "left" }}>
         .box{`{`}
         <br></br>
